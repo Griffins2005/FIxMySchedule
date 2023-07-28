16a2ec5aff81794fe4caa19c97d1ae5315ed2a02
Griffins2005 <134501193+Griffins2005@users.noreply.github.com>
2023-07-28 12:37:33 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/Login';
 import RegisterPage from './components/Register';
-import ProtectedRoute from './components/ProtectedRoute';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home/*" element={<ProtectedRoute />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
   );
